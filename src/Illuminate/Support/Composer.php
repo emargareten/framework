ec98084a94372f1a4904ad3c83482488d374cc59
Eliezer Margareten <46111162+emargareten@users.noreply.github.com>
2023-08-20 11:32:47 +0300

    Update Composer.php

--- src/Illuminate/Support/Composer.php
+++ src/Illuminate/Support/Composer.php
@@ -95,6 +95,26 @@ class Composer
     }
 
     /**
+     * Determine if the given Composer package is installed.
+     *
+     * @param  string  $package
+     * @return bool
+     */
+    protected function hasPackage($package)
+    {
+        $composerFile = "{$this->workingPath}/composer.json";
+
+        if (! file_exists($composerFile)) {
+            throw new RuntimeException("Unable to locate `composer.json` file at [{$this->workingPath}].");
+        }
+
+        $packages = json_decode(file_get_contents($composerFile), true);
+
+        return array_key_exists($package, $packages['require'] ?? [])
+            || array_key_exists($package, $packages['require-dev'] ?? []);
+    }
+
+    /**
      * Modify the "composer.json" file contents using the given callback.
      *
      * @param  callable(array):array  $callback
